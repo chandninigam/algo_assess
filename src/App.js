69860f7b2ecd44f115532a3a4b5974a944a59c4f
chandni nigam <nigamchandni139@gmail.com>
2023-03-14 17:14:32 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function onChangeFirst(e) {
+    console.log(e.target.value);
+    setFirstName(e.target.value);
+  }
+
+  function onChangeLast(e) {
+    setLastName(e.target.value);
+  }
+
+  function onClickSubmit() {
+    const obj = {
+      FirstName: firstName,
+      LastName: lastName,
+    };
+    console.log(obj);
+  }
+
+  function DynamicForm() {
+    return (
+      <>
+        <input
+          placeholder="FirstName"
+          // value={firstName}
+          onChange={onChangeFirst}
+        />
+        <input
+          placeholder="LastName"
+          // value={lastName}
+          onChange={onChangeLast}
+        />
+        <button onClick={onClickSubmit}>Sumbit</button>
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DynamicForm />
     </div>
   );
 }
